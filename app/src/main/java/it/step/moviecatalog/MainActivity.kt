ffb6d64ff639cb0b6ae30357c710f9281f5de0a4
AntonioLoSapio <antonio.losapio93s@gmail.com>
2023-09-18 09:42:13 +0200

    MainActivity - AppBar - Drawer

--- app/src/main/java/it/step/moviecatalog/MainActivity.kt
+++ app/src/main/java/it/step/moviecatalog/MainActivity.kt
@@ -2,10 +2,41 @@ package it.step.moviecatalog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var topAppBar : MaterialToolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        topAppBar = findViewById(R.id.topAppBar)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            topAppBar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(navigationView)
+        }
+
+
     }
 }
