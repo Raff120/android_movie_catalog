a2ecd7a80a8b0bec7696f097d3b6084270d250b1
Raff12 <cirillorf@gmail.com>
2023-09-21 10:39:52 +0200

    home_game_fragment

--- app/src/main/java/it/step/moviecatalog/fragment/HomeMovieFragment.kt
+++ app/src/main/java/it/step/moviecatalog/fragment/HomeMovieFragment.kt
@@ -6,11 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.divider.MaterialDividerItemDecoration
@@ -19,16 +17,14 @@ import it.step.moviecatalog.adapter.MovieAdapter
 import it.step.moviecatalog.databinding.FragmentHomeMovieBinding
 import it.step.moviecatalog.model.Movie
 import it.step.moviecatalog.viewmodel.MovieViewModel
-import java.net.ConnectException
 
 class HomeMovieFragment : Fragment() {
 
     private val movieViewModel: MovieViewModel by viewModels()
     private lateinit var movieAdapter: MovieAdapter
-    private var moviesList : List<Movie> = emptyList()
+    private var moviesList: List<Movie> = emptyList()
     private lateinit var bindingHomeMovies: FragmentHomeMovieBinding
-    private lateinit var view : View
-    private var listLoaded = true
+    private lateinit var view: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,14 +36,14 @@ class HomeMovieFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        if(isNetworkConnected(requireContext())) {
+        if (isNetworkConnected(requireContext())) {
 
             movieViewModel.initMovieList()
 
             // Inflate the layout for this fragment
             bindingHomeMovies = FragmentHomeMovieBinding.inflate(layoutInflater)
             view = bindingHomeMovies.root
-        }else{
+        } else {
             view = inflater.inflate(R.layout.no_connection_layout, container, false)
         }
 
@@ -57,7 +53,7 @@ class HomeMovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        if(isNetworkConnected(requireContext())){
+        if (isNetworkConnected(requireContext())) {
             val recyclerView: RecyclerView = view.findViewById(R.id.hmf_all_movies_recycler)
 
             // Create the observer which updates the UI.
@@ -65,22 +61,24 @@ class HomeMovieFragment : Fragment() {
                 // Update the UI
                 if (newMovieList != null) {
                     moviesList = newMovieList
-                    movieAdapter = MovieAdapter(moviesList){ movie ->
+                    movieAdapter = MovieAdapter(moviesList) { movie ->
                         //TODO on click
                     }
                     val layoutManager = LinearLayoutManager(requireContext())
                     recyclerView.layoutManager = layoutManager
                     recyclerView.adapter = movieAdapter
 
-                    if(newMovieList.isEmpty()) bindingHomeMovies.hmfMessage.text = "Lista vuota"
-                    else bindingHomeMovies.hmfMessage.text = ""
+                    if (newMovieList.isEmpty()) bindingHomeMovies.hmfMessage.text =
+                        getString(R.string.empty_list)
+                    else bindingHomeMovies.hmfMessage.text = getString(R.string.empty_string)
                 }
             }
 
             // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
             movieViewModel.moviesList.observe(viewLifecycleOwner, movieListObserver)
 
-            val divider = MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL )
+            val divider =
+                MaterialDividerItemDecoration(requireContext(), LinearLayoutManager.VERTICAL)
             recyclerView.addItemDecoration(divider)
         }
 
@@ -88,7 +86,8 @@ class HomeMovieFragment : Fragment() {
     }
 
     fun isNetworkConnected(context: Context): Boolean {
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connectivityManager.activeNetworkInfo
         return networkInfo != null && networkInfo.isConnected
     }
