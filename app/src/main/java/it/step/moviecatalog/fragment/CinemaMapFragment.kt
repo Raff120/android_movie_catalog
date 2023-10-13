0ed8a6b4ab018437548fb553b14b500d0db4e2a9
raffaele_cirillo <raffaele.cirillo@stepsrl.it>
2023-10-13 14:39:43 +0200

    feat_selected_markers

--- app/src/main/java/it/step/moviecatalog/fragment/CinemaMapFragment.kt
+++ app/src/main/java/it/step/moviecatalog/fragment/CinemaMapFragment.kt
@@ -12,6 +12,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.MutableLiveData
+import coil.load
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
@@ -20,11 +21,13 @@ import com.google.android.gms.location.LocationServices
 import it.step.moviecatalog.MainActivity
 import it.step.moviecatalog.R
 import it.step.moviecatalog.databinding.FragmentCinemaMapBinding
+import it.step.moviecatalog.enums.MarkerIcon
 import it.step.moviecatalog.model.Cinema
 import it.step.moviecatalog.viewmodel.CinemaViewModel
 import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.BoundingBox
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
@@ -44,6 +47,7 @@ class CinemaMapFragment : Fragment() {
     private var startPoint = MutableLiveData(GeoPoint(40.85715546623741, 14.277825536894902))
     private lateinit var mapController: IMapController
     private var marker: Marker? = null
+    private var markerSelected: Marker? = null
 
     private lateinit var mainActivity: Activity
 
@@ -94,6 +98,14 @@ class CinemaMapFragment : Fragment() {
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setMultiTouchControls(true)
         mapController = map.controller
+        // Imposta i limiti geografici per la Campania
+        val boundingBox = BoundingBox(
+            41.91022362928598,
+            16.703553394928914,
+            39.67012721608169,
+            11.96421761067527  // Latitudine e longitudine minime e massime per la Campania
+        )
+        map.setScrollableAreaLimitDouble(boundingBox)
         initMap()
 
         view = binding.root
@@ -139,12 +151,45 @@ class CinemaMapFragment : Fragment() {
 
 
     private fun addCinemaMarkers() {
-
         for (cinemaData in cinemaDataList) {
             val marker = Marker(map)
             marker.position = GeoPoint(cinemaData.latitude, cinemaData.longitude)
-            marker.icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_location)
+            marker.icon = ContextCompat.getDrawable(requireContext(), MarkerIcon.LOCATION_MARKER.id)
             marker.infoWindow = null
+
+            marker.setOnMarkerClickListener { marker, mapView ->
+                if (markerSelected == marker) {
+                    // Cliccando nuovamente sul marker deselezionerà il marker e nasconderà la card dei dettagli
+                    marker.icon =
+                        ContextCompat.getDrawable(requireContext(), MarkerIcon.LOCATION_MARKER.id)
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                    markerSelected = null
+                    binding.cinemaCard.visibility = View.GONE
+                } else {
+                    // Resetta il marker precedentemente selezionato (se presente)
+                    markerSelected?.icon =
+                        ContextCompat.getDrawable(requireContext(), MarkerIcon.LOCATION_MARKER.id)
+                    markerSelected?.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+
+                    // Seleziona il nuovo marker e mostra i dettagli del cinema
+                    marker.icon =
+                        ContextCompat.getDrawable(requireContext(), MarkerIcon.SELECTED_LOCATION_MARKER.id)
+                    marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+                    markerSelected = marker
+
+                    binding.cinemaImage.load(cinemaData.photo)
+                    binding.cinemaName.text = cinemaData.name
+                    binding.cinemaAddress.text = cinemaData.address
+                    binding.cinemaCard.visibility = View.VISIBLE
+                    mapController.zoomTo(16.0)
+                    
+                }
+
+                mapController.animateTo(GeoPoint(cinemaData.latitude, cinemaData.longitude))
+
+                true
+            }
+
             map.overlays.add(marker)
         }
 
@@ -192,8 +237,8 @@ class CinemaMapFragment : Fragment() {
             requestingLocationUpdates = true
             startLocationUpdates()
         }
-        mapController.setZoom(13.5)
-        mapController.setCenter(startPoint.value);
+        mapController.setZoom(13.5) //Valore ottimale 13.5
+        mapController.setCenter(startPoint.value)
         map.invalidate()
     }
 
@@ -201,15 +246,18 @@ class CinemaMapFragment : Fragment() {
         if (marker == null) {
             marker = Marker(map)
             marker!!.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-            marker!!.icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_my_location);
+            marker!!.icon = ContextCompat.getDrawable(requireContext(), MarkerIcon.MY_LOCATION_MARKER.id)
+            marker!!.infoWindow = null
             map.overlays.add(marker)
         }
         return marker!!
     }
 
-    private fun initFloatingButtons(){
+    private fun initFloatingButtons() {
         binding.gpsFloatingButton.setOnClickListener {
             readLastKnownLocation()
+            // Esegui un'animazione di zoom con un livello di zoom di 16.0
+            mapController.zoomTo(16.0)
         }
 
         binding.plusFloatingButton.setOnClickListener {
